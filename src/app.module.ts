5b70a828d3d51b516f3952b0770431a1081dd53a
Berserker8016 <cronobenji@gmail.com>
2024-06-28 16:47:24 -0600

    version 0.1.5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AllModule } from './all/all.module';
+import { HabitsModule } from './habits/habits.module';
+import { NotesModule } from './notes/notes.module';
+import { QuotesModule } from './quotes/quotes.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [AllModule, HabitsModule, NotesModule, QuotesModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
