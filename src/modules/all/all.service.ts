cdb7f10a83a888e43591d1138ed3842b3f4e20bc
eduardo.nila <eduardo.nila@teksi.mx>
2024-07-04 07:30:10 -0600

    ft:adding api service

--- src/modules/all/all.service.ts
+++ src/modules/all/all.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAllDto } from './dto/create-all.dto';
 import { UpdateAllDto } from './dto/update-all.dto';
+import { NotFoundException } from '@nestjs/common';
 
+export type AllTodo = any
 @Injectable()
 export class AllService {
-  create(createAllDto: CreateAllDto) {
-    return 'This action adds a new all';
+
+  private Alltodos:AllTodo = [
+    {
+      id:1,
+      userId: 1,
+      title: 'Clean the dishes',
+    },
+    {
+      id:2,
+      userId: 2,
+      title: 'Programming a bit',
+    },
+  ]
+  create(createAllDto: CreateAllDto): AllTodo {
+    const newTodo: AllTodo = {
+      id: this.Alltodos.length + 1,
+      ...createAllDto,
+    };
+    this.Alltodos.push(newTodo);
+    return newTodo;
+  }
+  findAll(): AllTodo[] {
+    return this.Alltodos;
   }
 
-  findAll() {
-    return `This action returns all all`;
+  findOne(id: number): AllTodo {
+    const todo = this.Alltodos.find(todo => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    return todo;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} all`;
+  update(id: number, updateAllDto: UpdateAllDto): AllTodo {
+    const todoIndex = this.Alltodos.findIndex(todo => todo.id === id);
+    if (todoIndex === -1) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    const updatedTodo = { ...this.Alltodos[todoIndex], ...updateAllDto };
+    this.Alltodos[todoIndex] = updatedTodo;
+    return updatedTodo;
   }
 
-  update(id: number, updateAllDto: UpdateAllDto) {
-    return `This action updates a #${id} all`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} all`;
+  remove(id: number): void {
+    const todoIndex = this.Alltodos.findIndex(todo => todo.id === id);
+    if (todoIndex === -1) {
+      throw new NotFoundException(`Todo with ID ${id} not found`);
+    }
+    this.Alltodos.splice(todoIndex, 1);
   }
 }
+ 
