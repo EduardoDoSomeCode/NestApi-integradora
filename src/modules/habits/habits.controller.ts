e1cd44eb32787a68834941e800f7f42cb1a03163
gururior <jesmanuel9991@gmail.com>
2024-07-11 14:41:38 -0600

    0.2.2 Se añade los comentarios de Habits

--- src/modules/habits/habits.controller.ts
+++ src/modules/habits/habits.controller.ts
@@ -8,27 +8,27 @@ import { Habits } from '@prisma/client';
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
 
-  @Post()
+  @Post() // La acción POST para crear un nuevo hábito en DTO
   createTask(@Body() newHabit: CreateHabitDto): Promise<Habits> {
     return this.habitsService.createHabitFromSample({ ...newHabit } as any);
   }
 
-  @Get()
+  @Get() // Para obtener todos los hábitos
   findAll(): Promise<Habits[]> {
     return this.habitsService.findAll();
   }
 
-  @Get(':id')
+  @Get(':id') // Es para obtener un hábito mediante por su ID
   findOne(@Param('id', ParseIntPipe) userId: bigint): Promise<Habits> {
     return this.habitsService.findOne(userId);
   }
 
-  @Patch(':id')
+  @Patch(':id') //Funciona para actualizar un hábito mediante por su ID
   update(@Param('id', ParseIntPipe) userId: bigint, @Body() UpdateHabitDto: UpdateHabitDto): Promise<Habits> {
     return this.habitsService.update(userId, UpdateHabitDto as any);
   }
 
-  @Delete(':id')
+  @Delete(':id') //Se ELimina un hábito mediante por su ID
   remove(@Param('id', ParseIntPipe) userId: bigint): Promise<Habits> {
     return this.habitsService.remove(userId);
   }
--- src/modules/habits/habits.service.ts
+++ src/modules/habits/habits.service.ts
@@ -2,14 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../common/utils/prisma.service';
 import { Prisma, Habits } from '@prisma/client';
 
-export interface Habit {
+export interface Habit { // Definición de la interfaz Habit
   id: number;
   text: string;
   date: Date;
 }
 
 @Injectable()
-export class HabitsService {
+export class HabitsService { //Se crea una Array de hábitos que se muestra
   private sampleHabits: Habit[] = [
     {
       id: 1,
@@ -28,32 +28,32 @@ export class HabitsService {
     },
   ];
 
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {} // Inyección del constructor privado para el servicio Prisma
 
   async createHabitFromSample(id: number): Promise<Habits> {
     const habitData = this.sampleHabits.find(habit => habit.id === id);
    
 
     try {
-      const newHabit = await this.prisma.habits.create({
+      const newHabit = await this.prisma.habits.create({ //Crea un nuevo hábito en la base de datos utilizando Prisma
         data: {
           title: habitData.text,
           recentDate: habitData.date,
-          userId: 1, // Ajusta el userId según tu lógica de negocio
+          userId: 1, // Ajusta el userId según tu lógica 
         },
       });
-      return newHabit;
+      return newHabit; //Una condición en caso de que no se crear el habito
     } catch (error) {
       console.error('Error al crear hábito:', error);
       throw new Error('No se pudo crear el hábito. Verifica los datos proporcionados.');
     }
   }
   
-  async findAll(): Promise<Habits[]> {
+  async findAll(): Promise<Habits[]> { //Es para obtener todos los hábitos 
     return this.prisma.habits.findMany();
   }
 
-  async findOne(idHabits: bigint): Promise<Habits> {
+  async findOne(idHabits: bigint): Promise<Habits> { //Es para obtener un hábito por su id
     const habit = await this.prisma.habits.findUnique({
       where: { idHabits },
     });
@@ -65,7 +65,7 @@ export class HabitsService {
     return habit;
   }
 
-  async update(idHabits: bigint, data: Prisma.HabitsUpdateInput): Promise<Habits> {
+  async update(idHabits: bigint, data: Prisma.HabitsUpdateInput): Promise<Habits> { // Para uno o varios campos en actualizar un hábito
     const habit = await this.prisma.habits.update({
       where: { idHabits },
       data,
@@ -78,7 +78,7 @@ export class HabitsService {
     return habit;
   }
   
-  async remove(idHabits: bigint): Promise<Habits> {
+  async remove(idHabits: bigint): Promise<Habits> { // Para eliminar un hábito meidante de su ID
     const habit = await this.prisma.habits.delete({
       where: { idHabits },
     });
