7f20d39a8d3df359b78068acbd646c13730c1a2f
eduardo.nila <eduardo.nila@teksi.mx>
2024-07-17 23:51:28 -0600

    ft:Migration service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,27 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/common/utils/prisma.service';
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findOne(userId: string): Promise<User | null> {
     return this.prisma.user.findUnique({
-      where: { IdUser:Number(userId) },
+      where: { IdUser: Number(userId) },
     });
   }
 
   async deleteUser(userId: number): Promise<void> {
     await this.prisma.user.delete({
-      where: { IdUser:Number(userId) },
+      where: { IdUser: Number(userId) },
     });
   }
 
-  async postNewUser(newUser: Omit<User, 'IdUser'>):Promise<User> {
-    return this.prisma.user.create({
+  async postNewUser(newUser: Omit<User, 'IdUser'>): Promise<any> {
+    if (newUser.email.length > 255) {
+      throw new BadRequestException('Email is too long');
+    }
+
+    // Validar longitud de otros campos si es necesario
+    if (newUser.name && newUser.name.length > 100) {
+      throw new BadRequestException('Name is too long');
+    }
+
+    const user = await this.prisma.user.create({
       data: newUser,
     });
 
+    // Convertir BigInt a string y tomar los primeros 3 números
+    return this.sanitizeUser(user);
+
+
+  }
+  private sanitizeUser(user: User) {
+    return {
+      ...user,
+      IdUser: user.IdUser.toString().slice(0, 3),
+    };
   }
 
 }
