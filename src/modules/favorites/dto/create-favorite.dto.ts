50a6b58fe60e614bf1aff1857d3f161ffc94032c
eduardo.nila <eduardo.nila@teksi.mx>
2024-07-18 04:59:25 -0600

    ft:favorites primas to user

--- src/modules/favorites/dto/create-favorite.dto.ts
+++ src/modules/favorites/dto/create-favorite.dto.ts
@@ -1,1 +1,7 @@
-export class CreateFavoriteDto {}
+import { IsNotEmpty, IsInt } from 'class-validator';
+
+export class CreateFavoriteDto {
+  @IsInt({ message: 'El ID de la nota debe ser un entero.' })
+  @IsNotEmpty({ message: 'Se requiere especificar el ID de la nota.' })
+  noteId: bigint; // Asegúrate de usar bigint si es necesario según tu base de datos
+}
