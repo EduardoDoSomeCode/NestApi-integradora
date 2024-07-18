b4fcfce7e28c3caec0b1bbb353556fa462ed9bd0
eduardo.nila <eduardo.nila@teksi.mx>
2024-07-18 01:01:59 -0600

    ft: adding changes to notes

--- src/modules/notes/dto/create-note.dto.ts
+++ src/modules/notes/dto/create-note.dto.ts
@@ -12,10 +12,10 @@ export class CreateNoteDto {
     @IsString({ message: "Se requiere que el contenido no este vacio"})
     @IsNotEmpty({ message: "El se requiere poner"})  
     body: string;   // Cuerpo o contenido de la nota
-    @IsInt({ message: "Se requiere no estar vacio"})
+    @IsInt({ message: "Se requiere que el ID no este vacio"})
     @IsNotEmpty({ message: "El se requiere poner"})
     IdUser: bigint;  // ID del usuario dueño de la nota (como BigInt)
-    @IsBoolean({ message: "Se reuiere marcar si es favorito o no"})
+    @IsBoolean({ message: "Se requiere marcar si es favorito o no"})
     @IsNotEmpty({ message: "El se requiere poner"})
     isFavorite?: boolean;   // Indicador opcional de si la nota es favorita
 }
--- src/modules/notes/notes.controller.ts
+++ src/modules/notes/notes.controller.ts
@@ -9,72 +9,94 @@ import { UpdateNoteDto } from './dto/update-note.dto';
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
-  // Ruta para crear una nueva entidad
+
+// Ruta para crear una nueva entidad
   @Post()
-  async create(@Body() createNoteDto: CreateNoteDto) {
+  async create(@Body() createNoteDto: CreateNoteDto): Promise<any> {
     try {
-      return await this.notesService.create(createNoteDto);
+
+      const result = await this.notesService.create(createNoteDto);
+
+      const response = JSON.stringify(result, (key, value) => {
+        // Convertir BigInt a string
+        if (typeof value === 'bigint') {
+          return value.toString();
+        }
+        return value;
+      });
+
+      return response
+      //return await this.notesService.create(createNoteDto);
     } catch (error) {
-      console.error('Error al crear la nota:', error.message); // Log del error
-      throw new InternalServerErrorException('Error al crear la nota');
+      throw new Error('Error al crear la nota');
     }
   }
 
-  // Ruta para obtener todas las entidades  
+// Ruta para obtener todas las entidades  
   @Get() 
   async findAll() {
     try {
       return await this.notesService.findAll();
     } catch (error) {
-      console.error('Error al obtener las notas:', error.message); // Log del error
+      console.log('Error al obtener las notas');
       throw new InternalServerErrorException('Error al obtener las notas');
+
     }
   }
 
-  // Ruta para obtener una entidad por su ID
+// Ruta para obtener una entidad por su ID
   @Get(':id')
   async findOne(@Param('id') id: string) {
     try {
       const noteId = BigInt(id);  // Convertir a BigInt
       const note = await this.notesService.findOne(noteId);
+
       if (!note) {
         throw new NotFoundException('Nota no encontrada');
       }
+
       return note;
     } catch (error) {
-      console.error('Error al obtener la nota:', error.message); // Log del error
+      console.log('Error al obtener la nota');
       throw new InternalServerErrorException('Error al obtener la nota');
+
     }
   }
 
-  // Ruta para actualizar una entidad por su ID
+// Ruta para actualizar una entidad por su ID
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
     try {
       const noteId = BigInt(id);  // Convertir a BigInt
+
       if (!updateNoteDto.title && !updateNoteDto.body && updateNoteDto.isFavorite === undefined) {
         throw new Error('Datos insuficientes para actualizar la nota');
       }
+
       return await this.notesService.update(noteId, updateNoteDto);
     } catch (error) {
-      console.error('Error al actualizar la nota:', error.message); // Log del error
+      console.log('Error al actualizar la nota');
       throw new InternalServerErrorException('Error al actualizar la nota');
+
     }
   }
 
-  // Ruta para eliminar una entidad por su ID
+// Ruta para eliminar una entidad por su ID
   @Delete(':id')
   async remove(@Param('id') id: string) {
     try {
       const noteId = BigInt(id);  // Convertir a BigInt
       const deletedNote = await this.notesService.remove(noteId);
+
       if (!deletedNote) {
         throw new NotFoundException('Nota no encontrada para eliminar');
       }
+
       return deletedNote;
     } catch (error) {
-      console.error('Error al eliminar la nota:', error.message); // Log del error
+      console.log('Error al eliminar la nota');
       throw new InternalServerErrorException('Error al eliminar la nota');
+
     }
   }
 }
