bfd23ba9f8515598167347f531c5d9447fbdf5dd
gururior <jesmanuel9991@gmail.com>
2024-07-18 13:06:52 -0600

    0.3.4 Creación de Validation Unique

--- src/modules/multimedias/dto/create-multimedia.dto.ts
+++ src/modules/multimedias/dto/create-multimedia.dto.ts
@@ -1,10 +1,24 @@
-import { IsString, Length } from 'class-validator';
+import { IsBoolean, IsInt, IsNotEmpty, IsString, IsUrl, Min } from 'class-validator';
 
 export class CreateMultimediaDto {
+    
+    @IsNotEmpty({ message: 'Este campo de la Id no este vacio' })
+    @IsInt({ message: 'Debe ser un entero.' })
+    @Min(1, { message: 'El ID debe ser un número positivo.' })
+    id: number;
 
-    id: BigInt;
+    @IsNotEmpty({ message: 'itIsImage no debe estar vacio' })
+    @IsBoolean({ message: 'Debe ser in boleano' })
     itIsImage: Boolean;
+
+    @IsNotEmpty({ message: 'Link no debe estar vacio' })
+    @IsString({ message: 'Debe ser un string' })
+    @IsUrl({}, { message: 'El Link sea valido' })
     link: String;
+
+    @IsNotEmpty({ message: ' El campo itIsSound no debe estar vacio' })
+    @IsString({ message: 'Debe ser un string' })
+    @IsUrl({}, { message: 'itIsSound que sea una URL validado' })
     itIsSound: String;
 
 
--- src/modules/multimedias/dto/update-multimedia.dto.ts
+++ src/modules/multimedias/dto/update-multimedia.dto.ts
@@ -1,10 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMultimediaDto } from './create-multimedia.dto';
-import { IsString, Length } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUrl, Length } from 'class-validator';
 
 export class UpdateMultimediaDto extends PartialType(CreateMultimediaDto) {
     
-    itIsImage?: Boolean;
-    link?: String;
-    itIsSound?: String;
+  @IsOptional()
+  @IsNotEmpty({ message: 'itIsImage no debe estar vacio' })
+  @IsBoolean({ message: 'Debe ser un booleano' })
+  itIsImage?: boolean;
+
+  @IsOptional()
+  @IsString({ message: 'Debe ser un string' })
+  @IsNotEmpty({ message: 'No debe estar vacio este campo' })
+  @IsUrl({}, { message: 'El Link debe ser válido' })
+  link?: string;
+
+  @IsOptional()
+  @IsString({ message: 'Debe ser un string' })
+  @IsNotEmpty({ message: 'No debe estar vacio este campo' })
+  @IsUrl({}, { message: 'itIsSound debe ser una URL válida' })
+  itIsSound?: string;
 }
