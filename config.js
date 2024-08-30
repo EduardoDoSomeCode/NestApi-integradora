379adbe3ac09885a2edb2c81898762a6ea5a6dad
eduardo.nila <eduardo.nila@teksi.mx>
2024-08-29 23:17:02 -0600

    change dev variable

--- config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = process.env.PORT || 3000
-export const DATABASE_URL = process.env.DATABASE_URL || ''
-export const DATABASE_HOST = process.env.DB_HOST || 'localhost'
-export const DATABASE_USER = process.env.DB_USER || 'root'
-export const DATABASE_PASSWORD = process.env.DB_USER || ''
